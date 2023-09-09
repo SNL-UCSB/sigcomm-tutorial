dafa7b6ce8b05c8f06a8e703ab3932ece530d5cc
maybe-hello-world <maybe.hello.world@gmail.com>
2023-09-09 10:28:24 -0700

    Update vm_prepare.sh

--- vm_prepare.sh
+++ vm_prepare.sh
@@ -2,7 +2,7 @@
 echo "ubuntu:sigcommtutorial" | chpasswd
 sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo systemctl restart sshd
-DEBIAN_FRONTEND=noninteractive apt-get update && apt-get upgrade -y && apt-get install -y python3-pip curl git
+DEBIAN_FRONTEND=noninteractive apt-get update && apt-get upgrade -y && apt-get install -y python3-pip curl git python3-venv
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
 
