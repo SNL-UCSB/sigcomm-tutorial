c4d349e61e0cbb5b38feb0913b45c816e1b9999d
maybe-hello-world <maybe.hello.world@gmail.com>
2023-09-09 13:48:48 -0400

    split preparation

--- vm_prepare.sh
+++ vm_prepare.sh
@@ -3,22 +3,8 @@ echo "ubuntu:sigcommtutorial" | chpasswd
 sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo systemctl restart sshd
 DEBIAN_FRONTEND=noninteractive apt-get update && apt-get upgrade -y && apt-get install -y python3-pip curl git python3-venv
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh ./get-docker.sh
-
-docker pull pinot.cs.ucsb.edu/cicflowmeter:latest
-docker pull pinot.cs.ucsb.edu/sigcommtutorial:latest
 
 git clone https://github.com/maybe-hello-world/sigcomm-tutorial.git /sigcommtutorial
 
-# prepare for session 3 - netunicorn
-cd /sigcommtutorial/session_3/netunicorn_practice && docker compose -f netunicorn-compose.yml up -d
-
-# install dependencies
-python3 -m venv /venv
-source /venv/activate
-python -m ipykernel install --user --name venv --display-name "sigcommtutorial"
-pip3 install -r /sigcommtutorial/requirements.txt
-
-# jupyter, password: sigcommtutorial
-jupyter lab --no-browser --ip=0.0.0.0 --port=80 --allow-root --KernelSpecManager.ensure_native_kernel=False --MultiKernelManager.default_kernel_name=venv --NotebookApp.password='argon2:$argon2id$v=19$m=10240,t=10,p=8$3XgMepqCMSodsKa9YtxXXg$5fjlph20kPgiDXYvPP47HIz1GmdLZg4ISvY5lPc+ILE' --notebook-dir=/sigcommtutorial/
+chmod +x /sigcommtutorial/vm_prepare_2.sh
+/bin/bash /sigcommtutorial/vm_prepare_2.sh
