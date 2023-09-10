0cf807b11e0eb42ef5eca189867352aafea7f42b
maybe-hello-world <maybe.hello.world@gmail.com>
2023-09-09 19:57:13 -0700

    Update vm_prepare_2.sh

--- vm_prepare_2.sh
+++ vm_prepare_2.sh
@@ -10,6 +10,7 @@ cd /sigcommtutorial/session_3/netunicorn && docker compose -f netunicorn-compose
 
 # install dependencies
 pip3 install -r /sigcommtutorial/requirements.txt
+pip3 install trustee
 
 # jupyter, password: sigcommtutorial
 jupyter lab --no-browser --ip=0.0.0.0 --port=8888 --allow-root --KernelSpecManager.ensure_native_kernel=False --MultiKernelManager.default_kernel_name=venv --NotebookApp.password='argon2:$argon2id$v=19$m=10240,t=10,p=8$3XgMepqCMSodsKa9YtxXXg$5fjlph20kPgiDXYvPP47HIz1GmdLZg4ISvY5lPc+ILE' --notebook-dir=/sigcommtutorial/
